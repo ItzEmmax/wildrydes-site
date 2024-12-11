f0d724410ec1dfc93998ffa137f2fa443e0b763e
ItzEmmax <132744199+ItzEmmax@users.noreply.github.com>
2024-12-11 22:10:07 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_240qYMtIi', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6kojiu9ni0qe0mq9b5hikpuat0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_ghIHj5ooC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'mf7j9rfificoche7urt6dd6vp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
